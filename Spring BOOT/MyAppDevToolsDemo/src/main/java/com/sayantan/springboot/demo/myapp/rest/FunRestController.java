d5c2a293d7e3e425484e4a88292b72b9b9db0db7
Sayantan <sayantansengupta98@gmail.com>
2020-08-29 22:05:46 +0530

    upto Running from CMD

---
+++ Spring BOOT/MyAppDevToolsDemo/src/main/java/com/sayantan/springboot/demo/myapp/rest/FunRestController.java
@@ -0,0 +1,30 @@
+package com.sayantan.springboot.demo.myapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// expose "/" that return "Hello World"
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run 50kms";
+	}
+
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Lucky Day";
+	}
+
+}
