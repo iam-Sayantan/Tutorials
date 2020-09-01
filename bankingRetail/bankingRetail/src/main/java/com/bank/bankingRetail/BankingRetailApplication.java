1183179fad5a0ef27b9ad7e772fff494988682f6
Sayantan <sayantansengupta98@gmail.com>
2020-09-01 08:50:39 +0530

    banking Retail (hard coded) - up to entity

---
+++ bankingRetail/bankingRetail/src/main/java/com/bank/bankingRetail/BankingRetailApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankingRetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingRetailApplication.class, args);
+	}
+
+}
---
+++ bankingRetail/bankingRetail/src/test/java/com/bank/bankingRetail/BankingRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankingRetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
